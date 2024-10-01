4538121f80c2b9b95a2139c3eb986495a97a9f8d
rifqiazizm <rifqiazizsmg@gmail.com>
2024-10-01 23:09:45 +0700

    kedua

---
+++ src/pages/playground/layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function layout() {
+  return (
+    <div className='fixed m-0 p-10 w-40 h-full bg-putih top-0 left-0'>
+        <div className='flex flex-row justify-start items-stretch w-full h-auto'>
+            <div className='h-20 p-4 text-center'>
+                <h1>Worst UI</h1>
+
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default layout
